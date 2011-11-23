cee2503f9c6416563adfbcb17c5a612b9d8f37ef
alswl <alswlx@gmail.com>
2011-11-23 23:49:19 +0800

    修改模型

--- model/dbevent.py
+++ model/dbevent.py
@@ -9,13 +9,14 @@ class Dbevent(db.Model):
     id = db.IntegerProperty(required=True)
     title = db.StringProperty(required=True)
     category = db.StringProperty(required=True) #TODO 关联对象
-    summary = db.StringProperty(required=True)
-    content = db.StringProperty #TODO
+    summary = db.StringProperty(required=True, multiline=True)
+    content = db.StringProperty(multiline=True) #TODO 是否必填
     invite_only = db.BooleanProperty() #需要邀请才能参加
     can_invite = db.BooleanProperty() #允许参加者邀请其友邻来参加
     participants = db.IntegerProperty() #参与人数
     wishers = db.IntegerProperty() #想参加的人
-    status = db.StringProperty() #想参加的人
+    album = db.StringProperty() #TODO 这个属性干嘛的？
+    status = db.StringProperty() #TODO 
     location_id = db.StringProperty(required=True) #城市id/拼音
     location_name = db.StringProperty(required=True) #城市/名称
     start_time = db.DateTimeProperty()
