44a9aab914d73605f9de0a0141b88279ca956bdc
alswl <alswlx@gmail.com>
2011-11-29 23:25:11 +0800

    去除demo残留文件

--- models.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python2
-#coding=utf-8
-
-from google.appengine.ext import db
-
-class TodoItem(db.Model):
-    author = db.UserProperty()
-    content = db.StringProperty()
-    created = db.DateTimeProperty(auto_now_add=True)
