748469d137ac3dee15a9b5d46daf613e3337e1e6
alswl <alswlx@gmail.com>
2011-11-29 14:48:05 +0800

    使用数据库存储数据

--- controller/event.py
+++ controller/event.py
@@ -6,6 +6,7 @@ import logging
 
 import web
 from google.appengine.api import users
+from google.appengine.ext import db
 from icalendar import Calendar, Event, UTC
 from google.appengine.api import urlfetch
 from BeautifulSoup import BeautifulStoneSoup
@@ -17,12 +18,11 @@ from model.dbevent import Dbevent
 
 routes = (
     '/test', 'Test',
-    '/location/add', 'Add',
     '/location/(.+)', 'Get',
 )
 
 apikey = '0a4b03a80958ff351ee10af81c0afd9f'
-event_type_map = {
+category_map = {
     'all': '所有类型',
     'music': '音乐/演出',
     'exhibition': '展览',
@@ -40,10 +40,10 @@ class Get:
     def GET(self, location):
         web.header('Content-Type', 'text/plain;charset=UTF-8')
         params = web.input(type='all') #web.py post/get默认值
-        event_type = params.type
-        if event_type not in event_type_map: #处理意外的type参数
-            event_type = 'all'
-        event_type = event_type.strip()
+        category = params.type
+        if category not in category_map: #处理意外的type参数
+            category = 'all'
+        category = category.strip()
 
         cal = Calendar()
         cal.add('prodid', '-//Google Inc//Google Calendar 70.9054//EN')
@@ -53,66 +53,44 @@ class Get:
         cal.add('METHOD', 'PUBLISH')
         cal.add('CALSCALE', 'GREGORIAN')
         cal.add('X-WR-CALNAME', '豆瓣%s - %s活动' \
-                %(location, event_type_map[event_type]))
+                %(location, category_map[category]))
         cal.add('X-WR-CALDESC',
                 'dbevent2gc - 豆瓣%s - %s活动 \n' \
                 'via http://dbevent2gc.appspot.com\n' \
                 'by alswl(http://log4d.com)' \
-                %(location, event_type_map[event_type]))
-        #cal.add('DTSTAMP', datetime.now())
+                %(location, category_map[category]))
         cal['dtstamp'] = datetime.strftime(datetime.now(), '%Y%m%dT%H%M%SZ')
 
-        #dbevents = Dbevent.all()
-        #dbevents.filter('location_id =', location)
-        #events = [dbevent2event(e) for e in dbevents]
+        dbevents = Dbevent.all() #从数据库获取数据
+        dbevents.filter('location_id =', location)
+        if category != 'all':
+            dbevents.filter('category =', 'event.' + category)
 
-        xml = fetchEvent(location, event_type=event_type) #TODO try...catch
+        if dbevents.count() == 0: #如果数据库没有值，则去实时查询
+            xml = fetchEvent(location, category=category)
+            dbevents_new = xml2dbevents(xml)
+            db.put(dbevents_new)
 
-        events = xml2dbevents(xml)
+        #豆瓣活动转换到iCalendar Event
+        events = [dbevent2event(e) for e in dbevents]
         for e in events:
-            cal.add_component(dbevent2event(e))
+            cal.add_component(e)
 
         return cal.as_string()
-
-class Add:
-    def GET(self):
-        dbevent = Dbevent(
-            id=id,
-            title=title,
-            self_link=self_link,
-            alternate_link=alternate_link,
-            category=category,
-            summary=summary,
-            content=content,
-            location_id=location_id,
-            location_name=location_name,
-            start_time=start_time,
-            end_time=end_time,
-            where=where,
-            )
-        dbevent.put()
-
-        return 'add ok'
 
 class Test:
     def GET(self):
         location = 'nanjing'
-        web.header('Content-Type', 'text/plain;charset=UTF-8')
+        category = 'all'
+        xml = fetchEvent(location, category=category, max=10)
+        dbevents_new = xml2dbevents(xml)
 
-        cal = Calendar()
-        cal.add('prodid', '-//My calendar product//mxm.dk//')
-        cal.add('version', '2.0')
-        cal.add('X-WR-CALDESC', u'豆瓣%s活动日历' %location)
-        cal.add('CLASS', 'PUBLIC')
-        cal.add('DESCRIPTION', u'dbevent2gc - 豆瓣%s活动日历' %location)
+        logging.info('len(dbevents_new)')
+        logging.info(len(dbevents_new))
 
-        xml = fetchEvent(location)
+        db.put(dbevents_new)
 
-        events = xml2dbevents(xml)
-        for e in events:
-            cal.add_component(dbevent2event(e))
-
-        return cal.as_string()
+        return 'ok'
 
 def dbevent2event(dbevent):
     """转换豆瓣数据模型到iCal数据模型"""
@@ -138,11 +116,11 @@ def dbevent2event(dbevent):
     event['uid'] = dbevent.id
     return event
 
-def fetchEvent(location, event_type='all', max=50):
+def fetchEvent(location, category='all', max=50):
     """从豆瓣api获取数据"""
     url = 'http://api.douban.com/event/location/%s?' \
             'type=%s&max-results=%d&apikey=%s' %(location,
-                                                event_type,
+                                                category,
                                                 max,
                                                 apikey)
     logging.info('fetch events from douban url: %s'%url)
@@ -156,7 +134,7 @@ def xml2dbevents(xml):
     """使用beautifulsoup转换html到Dbevent"""
     soup = BeautifulStoneSoup(xml)
     entrys = soup.findAll('entry')
-    events = []
+    events = [] #FIXME 名字修改
     for entry in entrys:
         events.append(entry2dbevent(entry))
     return events
@@ -166,11 +144,12 @@ def entry2dbevent(entry):
     self_link = entry.find('link', attrs={'rel': 'self'})['href']
     id = int(self_link.split('/')[-1])
 
-    title = entry.title.string
+    title = unicode(entry.title.string)
+    #logging.info(type(title))
     category = entry.category['term'].split('#')[-1]
     alternate_link = entry.find('link', attrs={'rel': 'alternate'})['href']
-    summary = entry.summary.string #.replace(r'\n', '</br>')
-    content = entry.content.string
+    summary = unicode(entry.summary.string) #.replace(r'\n', '</br>')
+    content = unicode(entry.content.string)
 
     if entry.find('db:attribute',
                   attrs={'name':'invite_only'}).string == 'yes':
@@ -190,12 +169,14 @@ def entry2dbevent(entry):
                                   attrs={'name':'album'}).string)
     location  = entry.find('db:location')
     location_id = location['id']
-    location_name = location.string
+    location_name = unicode(location.string)
+    #logging.info(type(location_name))
     start_time = iso8601.parse_date(entry.find('gd:when')['starttime'])
     end_time = iso8601.parse_date(entry.find('gd:when')['endtime'])
     where = entry.find('gd:where')['valuestring']
 
     dbevent = Dbevent(
+        key_name=str(id),
         id=id,
         title=title,
         self_link=self_link,
@@ -214,8 +195,7 @@ def entry2dbevent(entry):
 
     geo_point = entry.find('georss:point')
     if geo_point != None:
-        dbevent.geo_point = geo_point.string
-
+        dbevent.geo_point = unicode(geo_point.string)
 
     return dbevent
 
