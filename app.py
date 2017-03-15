25ecbd6f76d0cb44dfe13500e9f4dfdca85f887d
alswl <alswlx@gmail.com>
2017-03-15 22:34:28 +0800

    chore: google cloud platform

--- app.py
+++ app.py
@@ -3,13 +3,6 @@
 
 import sys
 
-#加入第三方包
-sys.path.insert(0, 'lib/BeautifulSoup-3.2.0-py2.5.egg')
-sys.path.insert(0, 'lib/web.py-0.36-py2.5.egg')
-sys.path.insert(0, 'lib/icalendar-2.2-py2.5.egg')
-sys.path.insert(0, 'lib/Mako-0.5.0-py2.5.egg')
-sys.path.insert(0, 'lib/iso8601-0.1.4-py2.5.egg')
-
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 import web
